a866fc5f7572bc263ba63632381f3441ee3d99ff
Rishab Chakrabarti <73683708+TheSaltisHere@users.noreply.github.com>
2022-03-07 12:44:27 +0530

    Time: 212 ms (89.13%), Space: 16.8 MB (8.40%) - LeetHub

---
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.py
@@ -0,0 +1,18 @@
+from itertools import permutations
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        arr_odd=[]
+        arr_even=[]
+        for i in nums:
+            if i%2==0:
+                arr_even.append(i)
+            else:
+                arr_odd.append(i)
+        final=[]
+        while len(arr_odd)!=0 or len(arr_even)!=0:
+            final.append(arr_even.pop())
+            final.append(arr_odd.pop())
+        print(final)
+        return final
+            
+        
